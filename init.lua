8d8027b705eda02ce728645321811529c7429c37
mithys <mithys@gmail.com>
2022-04-15 17:19:56 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,226 @@
+local config = {
+
+ -- Set colorscheme
+  colorscheme = "default_theme",
+
+  -- Default theme configuration
+  default_theme = {
+    diagnostics_style = "none",
+    -- Modify the color table
+    colors = {
+      fg = "#abb2bf",
+    },
+    -- Modify the highlight groups
+    highlights = function(highlights)
+      local C = require "default_theme.colors"
+
+      highlights.Normal = { fg = C.fg, bg = C.bg }
+      return highlights
+    end,
+  },
+
+  -- Disable default plugins
+  enabled = {
+    bufferline = true,
+    neo_tree = true,
+    lualine = true,
+    gitsigns = true,
+    colorizer = true,
+    toggle_term = true,
+    comment = true,
+    symbols_outline = true,
+    indent_blankline = true,
+    dashboard = true,
+    which_key = true,
+    neoscroll = true,
+    ts_rainbow = true,
+    ts_autotag = true,
+  },
+
+  -- Disable AstroVim ui features
+  ui = {
+    nui_input = true,
+    telescope_select = true,
+  },
+
+  -- Configure plugins
+  plugins = {
+    -- Add plugins, the packer syntax without the "use"
+    init = {
+      {
+        "Mofiqul/dracula.nvim",
+        as = "dracula",
+        config = function()
+          vim.cmd "colorscheme dracula"
+        end,
+       },
+      {
+        "phaazon/hop.nvim",
+        branch = "v1", -- optional but strongly recommended
+        config = function()
+          -- you can configure Hop the way you like here; see :h hop-config
+          require"hop".setup { keys = "dkslagheiruwoqptycvmxzbn" }
+        end
+      },
+      {
+        "tpope/vim-unimpaired"
+      },
+      {
+        "bkad/CamelCaseMotion"
+      },
+      {
+        "Saecki/crates.nvim",
+        after = "nvim-cmp",
+        config = function()
+          require("crates").setup()
+
+          local cmp = require "cmp"
+          local config = cmp.get_config()
+          table.insert(config.sources, { name = "crates" })
+          cmp.setup(config)
+        end,
+      },
+      -- { "andweeb/presence.nvim" },
+      -- {
+      --   "ray-x/lsp_signature.nvim",
+      --   event = "BufRead",
+      --   config = function()
+      --     require("lsp_signature").setup()
+      --   end,
+      -- },
+    },
+    -- All other entries override the setup() call for default plugins
+    treesitter = {
+      ensure_installed = { "lua" },
+    },
+    packer = {
+      compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
+    },
+  },
+
+  -- Add paths for including more VS Code style snippets in luasnip
+  luasnip = {
+    vscode_snippet_paths = {},
+  },
+
+  -- Modify which-key registration
+  ["which-key"] = {
+    -- Add bindings to the normal mode <leader> mappings
+    register_n_leader = {
+      -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+    },
+  },
+
+  -- Extend LSP configuration
+  lsp = {
+    -- add to the server on_attach function
+    -- on_attach = function(client, bufnr)
+    -- end,
+
+    -- override the lsp installer server-registration function
+    -- server_registration = function(server, opts)
+    --   server:setup(opts)
+    -- end
+
+    -- Add overrides for LSP server settings, the keys are the name of the server
+    ["server-settings"] = {
+      -- example for addings schemas to yamlls
+      -- yamlls = {
+      --   settings = {
+      --     yaml = {
+      --       schemas = {
+      --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+      --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+      --       },
+      --     },
+      --   },
+      -- },
+    },
+  },
+
+  -- Diagnostics configuration (for vim.diagnostics.config({}))
+  diagnostics = {
+    virtual_text = true,
+    underline = true,
+  },
+
+  -- null-ls configuration
+  ["null-ls"] = function()
+    -- Formatting and linting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim
+    local status_ok, null_ls = pcall(require, "null-ls")
+    if not status_ok then
+      return
+    end
+
+    -- Check supported formatters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    local formatting = null_ls.builtins.formatting
+
+    -- Check supported linters
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    local diagnostics = null_ls.builtins.diagnostics
+
+    null_ls.setup {
+      debug = false,
+      sources = {
+        -- Set a formatter
+        formatting.rufo,
+        -- Set a linter
+        diagnostics.rubocop,
+      },
+      -- NOTE: You can remove this on attach function to disable format on save
+      on_attach = function(client)
+        if client.resolved_capabilities.document_formatting then
+          vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+        end
+      end,
+    }
+  end,
+  
+  -- Remap to Hop
+  ["which-key"] = {
+    register_n_leader = {
+      ["w"] = { "<cmd>HopWord<cr>", "Hop Word" },
+      ["b"] = { "<cmd>HopWord<cr>", "Hop Word" },
+      ["j"] = { "<cmd>HopLine<cr>", "Hop Line" },
+      ["k"] = { "<cmd>HopLine<cr>", "Hop Line" },
+    },
+  },
+
+  -- This function is run last
+  -- good place to configure mappings and vim options
+  polish = function()
+    local opts = { noremap = true, silent = true }
+    local map = vim.api.nvim_set_keymap
+    local set = vim.opt
+    -- Set options
+    set.relativenumber = false
+
+    -- Set key bindings
+    -- map("n", "<C-s>", "<cmd>w!<cr>", opts)
+    map("n", "/", "<cmd>noh<cr>/", opts)
+
+    map("", "<leader>w", "<cmd>HopWord<cr>", opts)
+    map("", "<leader>b", "<cmd>HopWord<cr>", opts)
+    map("", "<leader>j", "<cmd>HopLine<cr>", opts)
+    map("", "<leader>k", "<cmd>HopLine<cr>", opts)
+
+    map("", "f", "<cmd>HopChar1<cr>", opts)
+
+    map("", "W", "<Plug>CamelCaseMotion_w", opts)
+    map("", "B", "<Plug>CamelCaseMotion_b", opts)
+    map("", "E", "<Plug>CamelCaseMotion_e", opts)
+
+    -- Set autocommands
+    vim.cmd [[
+      augroup packer_conf
+        autocmd!
+        autocmd bufwritepost plugins.lua source <afile> | PackerSync
+      augroup end
+    ]]
+  end,
+}
+
+return config
